2e30b4c233ec2fd5170437f7b4dc7e53ae395d07
Corey Acri <acri.corey@gmail.com>
2018-01-21 02:38:10 -0500

    taking out routes for now

--- routes/web.php
+++ routes/web.php
@@ -16,12 +16,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/test', 'MentorTwilioController@test'); 
+
+
+
+// Route::get('/test', 'MentorTwilioController@test'); 
 
 
 //this is where the form goes, the form then call the post -> store 
-Route::get('/newnumber/create', 'IncomingMessageController@create');
+// Route::get('/newnumber/create', 'IncomingMessageController@create');
 
 
 //set up the route that will be used by the post->store 
-Route::post('/newnumber','IncomingMessageController@store'); 
+// Route::post('/newnumber','IncomingMessageController@store'); 
