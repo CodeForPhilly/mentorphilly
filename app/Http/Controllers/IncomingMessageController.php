1cd2a85e80e05c3350a9c947e969e339e617429b
Corey Acri <acri.corey@gmail.com>
2018-01-23 14:47:28 -0500

    adding receiving text message

--- app/Http/Controllers/IncomingMessageController.php
+++ app/Http/Controllers/IncomingMessageController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-use App\IncomingMessage; 
 use App\Notifications\IncomingTextMessage; 
 
 use Twilio; 
--- app/IncomingMessage.php
+++ app/IncomingMessage.php
@@ -1,8 +1,7 @@
 <?php
 
 namespace App;
-
-
+use Illuminate\Notifications\Notifiable;
 
 class IncomingMessage extends Model
 {
