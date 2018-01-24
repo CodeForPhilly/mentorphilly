a71d68980cef4e88831afa98738b1b255fcc6531
Corey Acri <acri.corey@gmail.com>
2018-01-24 07:48:14 -0500

    putting hook in env variable

--- app/User.php
+++ app/User.php
@@ -30,6 +30,6 @@ class User extends Authenticatable
 
     public function routeNotificationForSlack(){
 
-        return config(services.slack.hook);
+        return config('services.slack.hook');
     }
 }
