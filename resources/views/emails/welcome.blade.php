7d4d2c9505b697c0b55d52b89dba3a42f5ea8050
Corey Acri <acri.corey@gmail.com>
2018-02-09 20:45:30 -0500

    testing email on new reg

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -7,7 +7,7 @@
 
 	<h1>Welcome to MentorPhilly</h1>
 	<p>You've registered as an admin of the Mentor Philly SMS app</p>
-	<p>Your user name is: {{$name}}</p>
+	<p>Your user name is: {{$email}}</p>
 	<p>Your password is: {{$password}}</p>
 
 
