79c8877f1c4a04358306a382e84a34abb6bd2bf1
Corey Acri <acri.corey@gmail.com>
2018-02-10 14:03:49 -0500

    trying to pass data to view

--- app/Mail/WelcomeRegisteredUser.php
+++ app/Mail/WelcomeRegisteredUser.php
@@ -11,17 +11,14 @@ class WelcomeRegisteredUser extends Mailable
 {
     use Queueable, SerializesModels;
 
-
-    public $data; 
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct($data)
+    public function __construct()
     {
         //
-        $this->data = $data; 
     }
 
     /**
