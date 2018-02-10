5d1533ca78a50972cf38b2f93d0a187fe7c3cb82
Corey Acri <acri.corey@gmail.com>
2018-02-10 14:17:48 -0500

    trying to pass data to view

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -90,9 +90,9 @@ class RegisterController extends Controller
             'password' => bcrypt($data['password']),
         ]);
 
-        $welcome_data = ['email' => $user->email,'password' => $user->password];
+        // $welcome_data = ['email' => $user->email,'password' => $user->password];
 
-        \Mail::to($user)->send(new WelcomeRegisteredUser(), $welcome_data); 
+        \Mail::to($user)->send(new WelcomeRegisteredUser()); 
 
         // return User::create([
         //     'name' => $data['name'],
