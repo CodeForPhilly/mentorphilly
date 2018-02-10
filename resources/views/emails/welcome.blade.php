5c171a4d94e6001fb2c1b9a402271da7638402cf
Corey Acri <acri.corey@gmail.com>
2018-02-10 14:05:06 -0500

    trying to pass data to view

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -7,8 +7,8 @@
 
 	<h1>Welcome to MentorPhilly</h1>
 	<p>You've registered as an admin of the Mentor Philly SMS app</p>
-	<p>Your user name is: {{$data['email']}}</p>
-	<p>Your password is: {{$data['password']}}</p>
+	<p>Your user name is: {{$welcome_data['email']}}</p>
+	<p>Your password is: {{$welcome_data['password']}}</p>
 
 
 </body>
