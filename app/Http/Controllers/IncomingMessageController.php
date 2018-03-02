de0d64a35cd3855e08e9ff3043fab4e7f9dd3d14
Corey Acri <acri.corey@gmail.com>
2018-03-02 00:08:21 -0500

    trying to get rid of offset zero error

--- app/Http/Controllers/IncomingMessageController.php
+++ app/Http/Controllers/IncomingMessageController.php
@@ -94,7 +94,7 @@ class IncomingMessageController extends Controller
       	if(null != $request->input('From')){
       		$from = $request->input('From');
       		$mentees = $this->checkForMentee($from);  
-          if(!empty($mentees))
+          if(!empty($mentees[0]))
       		  $mentee = $mentees[0]->smsname;
       	}
 
