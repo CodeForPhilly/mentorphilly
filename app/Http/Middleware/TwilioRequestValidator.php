2423cb18b94dc18feeeb848088f93b9583f00a9c
Corey Acri <acri.corey@gmail.com>
2018-03-01 22:36:03 -0500

    adding request validator path extension

--- app/Http/Middleware/TwilioRequestValidator.php
+++ app/Http/Middleware/TwilioRequestValidator.php
@@ -20,7 +20,7 @@ class TwilioRequestValidator
     {
          // Be sure TWILIO_APP_TOKEN is set in your .env file.
       // You can get your app token in your twilio console https://www.twilio.com/console
-      $requestValidator = new RequestValidator(env('TWILIO_APP_TOKEN'));
+      $requestValidator = new \Services_Twilio_RequestValidator(env('TWILIO_APP_TOKEN'));
 
       $isValid = $requestValidator->validate(
         $request->header('X-Twilio-Signature'),
