6396a3a70977e51d7dc2e8257e26009a181f42e9
Corey Acri <acri.corey@gmail.com>
2018-04-02 16:16:40 -0400

    reset

--- app/Http/Controllers/IncomingMessageController.php
+++ app/Http/Controllers/IncomingMessageController.php
@@ -120,7 +120,7 @@ class IncomingMessageController extends Controller
 
 
     //send auto reply if the number hasn't text us before 
-   // if(!IncomingMessage::where('number', '=', $message->incoming_number)->first())
+   // if(!IncomingMessage::where('number', '=', $message->incoming_number)->exists())
    //  Twilio::message($message->incoming_number, 'Welcome to MentorPhilly! Someone will respond to you within 24 hours.');
 
     
