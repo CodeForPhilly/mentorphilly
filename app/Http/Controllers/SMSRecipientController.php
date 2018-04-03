5aeae84b92050e0479a10f96ca720c36396479d9
Corey Acri <acri.corey@gmail.com>
2018-04-03 12:55:17 -0400

    adding hash in front

--- app/Http/Controllers/SMSRecipientController.php
+++ app/Http/Controllers/SMSRecipientController.php
@@ -41,13 +41,14 @@ class SMSRecipientController extends Controller
 
 	function cleanChannelName($string) {
     //Lower case everything
-	    $string = "#".strtolower($string); 
+	    $string = strtolower($string); 
 	    //Make alphanumeric (removes all other characters)
 	    $string = preg_replace("/[^a-z0-9_\s-]/", "", $string);
 	    //Clean up multiple dashes or whitespaces
 	    $string = preg_replace("/[\s-]+/", " ", $string);
 	    //Convert whitespaces and underscore to dash
 	    $string = preg_replace("/[\s_]/", "-", $string);
+	    $string = "#" . $string; 
 
 
 	    return $string;
