e4ada3ceaa8e3b5ed7685fb1b4078bcecf0bebc3
Corey Acri <acri.corey@gmail.com>
2018-04-03 12:30:42 -0400

    checkfor phone

--- app/Http/Controllers/IncomingMessageController.php
+++ app/Http/Controllers/IncomingMessageController.php
@@ -113,7 +113,7 @@ class IncomingMessageController extends Controller
     $phone = new Phone(); 
 
    //  //check if we already have this number in the db
-    $phone = $this->checkForPhone($message, $phone);
+    // $phone = $this->checkForPhone($message, $phone);
 
    //  //if we have that phone (!null) then update the message with the corresponding name
    //  if($phone != null)
