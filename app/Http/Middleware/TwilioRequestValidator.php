3b764646a6e99fd25ee417dafd3e7025a10a1d71
Corey Acri <acri.corey@gmail.com>
2018-04-08 14:48:03 -0400

    cleaning up

--- app/Http/Middleware/TwilioRequestValidator.php
+++ app/Http/Middleware/TwilioRequestValidator.php
@@ -13,12 +13,12 @@ use Illuminate\Http\Response;
 
 class TwilioRequestValidator 
 {
-      
-     protected $except = [
-        'fromslack',
-        'incomingMessageTest', 
-        'incomingMessage'
-      ]; 
+
+ protected $except = [
+  'fromslack',
+  'incomingMessageTest', 
+  'incomingMessage'
+]; 
     /**
      * Handle an incoming request.
      *
@@ -30,7 +30,7 @@ class TwilioRequestValidator
     {
          // Be sure TWILIO_APP_TOKEN is set in your .env file.
       // You can get your app token in your twilio console https://www.twilio.com/console
-      
+
 
 
       $requestValidator = new \Services_Twilio_RequestValidator(env('TWILIO_TOKEN'));
@@ -42,25 +42,21 @@ class TwilioRequestValidator
       );
 
 
+      //Log the request
       \Log::info('Incoming Message On:' . date('m/d/Y h:i:s a', time()) . "from url: " . $request->fullUrl());
 
-     
+      try {
+        if ($isValid) {
+         //log that it is valid
+         \Log::info('  Validated: ' . date('  m/d/Y h:i:s a  ', time()) . "  from url:  " . $request->fullUrl());
+         return $next($request);
+       }
+     } catch (Exception $e) {
+      report($e);
 
-      
-
-           try {
-        // Validate the value...
-             if ($isValid) {
-
-               \Log::info('  Validated: ' . date('  m/d/Y h:i:s a  ', time()) . "  from url:  " . $request->fullUrl());
-        return $next($request);
-      }
-    } catch (Exception $e) {
-        report($e);
-
-        return false;
+      return false;
     }
 
-      
-    }
+
+  }
 }
